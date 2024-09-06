b681f093d0c0d3046393be52d617e6893ecc1cfa
marcellepaiva <marcelle.nicole.paiva@gmail.com>
2024-09-06 15:48:15 -0300

    aula 09

---
+++ aula-09.js
@@ -0,0 +1,10 @@
+function soma (item1, item2) {
+  return item1 + item2;
+}
+
+function diminuir (item1, item2) {
+    return item1 - item2; 
+}
+
+console.log('SOMA: ', soma(5,6));
+console.log('SOMA: ', diminuir(50,100));
