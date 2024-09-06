6ad04ee2e8a9a215c06a3c975f94bbef3cf7a895
marcellepaiva <marcelle.nicole.paiva@gmail.com>
2024-09-06 11:28:16 -0300

    Aulas 02, 03, e 04 adicionadas

---
+++ aula-03.js
@@ -0,0 +1,12 @@
+const idade = 22; 
+const nome = 'Marcelle';
+
+if (idade >= 18 && nome === 'Marcelle') {
+    console.log('ENTROU!!');
+    
+} else {
+    console.log('Não entra, idade não permitida :(');
+}
+
+
+// um = é atribuição, dois == é um comparativo e === é um comparativo mais preciso
